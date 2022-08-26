f7bb6478948129f9229eccb8287f11e081db0fce
yoojenogsik <vnfthr@naver.com>
2022-08-26 15:09:34 +0900

    Initial commit

---
+++ src/main/java/com/example/restful/helloworld/HelloWorldBean.java
@@ -0,0 +1,7 @@
+package com.example.restful.helloworld;
+
+import lombok.Data;
+
+@Data
+public class HelloWorldBean {
+}
---
+++ src/main/java/com/example/restful/helloworld/HelloWorldController.java
@@ -0,0 +1,7 @@
+package com.example.restful.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+}
